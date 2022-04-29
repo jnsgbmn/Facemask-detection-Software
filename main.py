d1450ad385726a9200495a47b929136178498a6f
joangumban21 <jgumban@mcm.edu.ph>
2022-04-29 21:02:04 +0800

    initial commit

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
-from PyQt5 import QtGui, QtCore, Qt, uic
-from PyQt5.QtCore import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt, QPropertyAnimation, QThread, pyqtSignal, pyqtSlot)
+
+from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient, QImage)
+
+
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
 from PyQt5.uic import loadUiType
-import os
+
+
+
+
 import sys
-import time
-
-from matplotlib import widgets
-from matplotlib.pyplot import connect
-
-
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from os.path import dirname, join
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import load_model
-from imutils.video import VideoStream
-import numpy as np
-import imutils
-import time
 import cv2
+import numpy as np
 import os
+import pandas as pd
+from tensorflow import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+import pickle
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPool2D
+from keras.layers import Flatten
+from keras.layers import Dense
+import time
+from datetime import datetime
+from PIL.ImageQt import ImageQt
+from PIL import Image
+
+counter = 0
+GLOBAL_STATE = 0
 
 
 class ThreadProgress(QThread):
@@ -41,77 +56,6 @@ FROM_Main,_ = loadUiType(os.path.join(os.path.dirname(__file__),"Fcamera.ui"))
 
 
 
-class detect_mask():
-    def videosurvillance (self):
-        def detect_and_predict_mask(frame, faceNet, maskNet):
-            (h, w) = frame.shape[:2]
-            blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
-                                    (104.0, 177.0, 123.0))
-            faceNet.setInput(blob)
-            detections = faceNet.forward()
-            print(detections.shape)
-            faces = []
-            locs = []
-            preds = []
-
-            for i in range(0, detections.shape[2]):
-                confidence = detections[0, 0, i, 2]
-                if confidence > 0.5:
-                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                    (startX, startY, endX, endY) = box.astype("int")
-
-                    (startX, startY) = (max(0, startX), max(0, startY))
-                    (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
-
-                    face = frame[startY:endY, startX:endX]
-                    face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-                    face = cv2.resize(face, (224, 224))
-                    face = img_to_array(face)
-                    face = preprocess_input(face)
-
-                    faces.append(face)
-                    locs.append((startX, startY, endX, endY))
-            if len(faces) > 0:
-                faces = np.array(faces, dtype="float32")
-                preds = maskNet.predict(faces, batch_size=32)
-                return (locs, preds)
-        prototxtPath = r"deploy.protext"
-        weightsPath = r"res10_300x300_ssd_iter_140000.caffemodel"
-        faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
-
-        maskNet = load_model("mask_detector.model")
-        print("Starting the CAMERA...")
-        
-
-        while True:
-            frame = vs.read()
-            frame = imutils.resize(frame)
-            (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
-            for (box, pred) in zip(locs, preds):
-
-                (startX, startY, endX, endY) = box
-                (mask, withoutMask) = pred
-
-                label = "With Mask" if mask > withoutMask else "No Mask"
-                color = (0, 255, 0) if label == "With Mask" else (0, 0, 255)
-                label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-                cv2.putText(frame, label, (startX, startY - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-                cv2.rectangle(frame, (startX, startY), (endX, endY), color, 5)
-
-    def convertFrame(self):
-        try:
-            height,width = self.frame.shape[:2]
-            img=QtGui.QImage(self.currentFrame,
-                             width,
-                              height,
-                             QtGui.QImage.Format_RGB888)
-            img=QtGui.QPixmap.fromImage(img)
-            self.previousFrame = self.currentFrame
-            return img
-        except:
-            return None
-
 
 
 class MainWindow(QMainWindow, FROM_Main):
@@ -131,26 +75,38 @@ class MainWindow(QMainWindow, FROM_Main):
         self.ui.btnrecog.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.recog))
         self.ui.btndatabes.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.Database))
 
-        self._timer = QtCore.QTimer(self)
-        self._timer.timeout.connect(self.play)
-        self.update()
 
-    def play(self):
-         try:
-            self.video.videosurvillance()
-            self.videoFrame.setPixmap(self.video.convertFrame())
-            self.videoFrame.setScaledContents(True)
-         except TypeError:
-            print ("No frame")
-
-   
-
-    
-    
+        self.logic = 0
+        self.value = 1
+        self.ui.btncam.clicked.connect(self.onClicked)
 
 
-        
-
+    @pyqtSlot()
+    def onClicked(self):
+        cap = cv2.VideoCapture(0)
+        while cap.isOpened():
+            ret,frame = cap.read()
+            frame = cv2.flip(frame,1)
+            if ret == True:
+                self.displayImage(frame,1)
+                cv2.waitKey()
+                if self.logic2 == 2 :
+                    self.value2 = self.value2 +1
+                    cv2.imwrite()
+                    self.logic2 = 1
+        cap.release()
+        cv2.destroyAllWindows()
+    def displayImage(self,img,window=1):
+        qformat = QImage.Format_Indexed8
+        if len(img.shape) == 3:
+            if img.shape[2] == 4:
+                qformat =QImage.Format_RGBA888
+            else:
+                qformat = QImage.Format_RGB888
+        img = QImage(img, img.shape[1],img.shape[0],qformat)
+        img = img.rgbSwapped()
+        self.ui.recognition_page_label.setPixmap(QPixmap.fromImage(img))
+        self.ui.recognition_page_label.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
 
 class SplashScreen(QMainWindow, FROM_SPLASH):
     def __init__(self, parent = None):
@@ -179,14 +135,6 @@ class SplashScreen(QMainWindow, FROM_SPLASH):
 
         
     
-def main():
-    app=QApplication(sys.argv)
-    window = SplashScreen()
-    window.show()
-    app.exec_()
-
-if __name__ == '__main__':
-    try:
-        main()
-    except Exception as why:
-        print(why)
+app = QtWidgets.QApplication(sys.argv)
+window = SplashScreen()
+app.exec_()
